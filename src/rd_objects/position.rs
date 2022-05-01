cec0acfe7f14f948970241f0765ba256c6cc2fc2
lonnonjamesd <lemonjameslem@protonmail.com>
2022-05-01 19:43:54 -0400

    added a generic macro for non-repetitive code

--- src/rd_objects/position.rs
+++ src/rd_objects/position.rs
@@ -6,21 +6,53 @@ use graphics::math::Scalar;
 pub trait Girthy {
     fn getDimensions(&self) -> (Scalar, Scalar);
 
-    fn resize(&mut self, dw: Scalar, dh: Scalar) -> (Scalar, Scalar);
-
-    fn setH(&mut self, h: Scalar);
-    fn setW(&mut self, w: Scalar);
+    fn resize_by(&mut self, dw: Scalar, dh: Scalar);
+    fn resize(&mut self, w: Scalar, h: Scalar);
 }
 
 /**
 * Rules for how things should be positioned.
 */
 pub trait Positioned {
-    fn shiftPosition(&self, dx: Scalar, dy: Scalar) -> (Scalar, Scalar);
+    fn shiftPosition(&mut self, dx: Scalar, dy: Scalar);
 
     fn getPosition(&self) -> (Scalar, Scalar);
     fn setPosition(&mut self, x: Scalar, y: Scalar);
+}
 
-    fn setX(&mut self, x: Scalar);
-    fn setY(&mut self, y: Scalar);
+macro_rules! impl_generic_traits {
+    (Girthy for $x:ident) => {
+        impl Girthy for Entity {
+            fn getDimensions(&self) -> (Scalar, Scalar) {
+                (self.w, self.h)
+            }
+
+            fn resize_by(&mut self, dw: Scalar, dh: Scalar) {
+                self.h += dh;
+                self.w += dw;
+            }
+
+            fn resize(&mut self, w: Scalar, h: Scalar) {
+                self.h = h;
+                self.w = w;
+            }
+        }
+    };
+    (Position for $x:ident) => {
+        impl Positioned for Entity {
+            fn shiftPosition(&mut self, dx: Scalar, dy: Scalar) {
+                self.x += dx;
+                self.y += dy;
+            }
+
+            fn getPosition(&self) -> (Scalar, Scalar) {
+                (self.x, self.y)
+            }
+
+            fn setPosition(&mut self, x: Scalar, y: Scalar) {
+                self.x = x;
+                self.y = y;
+            }
+        }
+    };
 }
