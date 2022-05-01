e14bb3b6c7d76f469b9a6147588013ec913193e0
lonnonjamesd <lemonjameslem@protonmail.com>
2022-05-01 19:30:46 -0400

    added more colors

--- src/rd_utils/colors.rs
+++ src/rd_utils/colors.rs
@@ -1,14 +1,16 @@
 use graphics::types::{Color, ColorComponent};
 
-pub const TRANSPARENT: Color = [0.0, 0.0, 0.0, 0.0];
-pub const YELLOW: Color = [1.0, 1.0, 0.0, 1.0];
-pub const BLACK: Color = [0.0, 0.0, 0.0, 1.0];
-pub const GREEN: Color = [0.0, 1.0, 0.0, 1.0];
-pub const WHITE: Color = [1.0, 1.0, 1.0, 1.0];
-pub const BLUE: Color = [0.0, 0.0, 1.0, 1.0];
 pub const RED: Color = [1.0, 0.0, 0.0, 1.0];
+pub const YELLOW: Color = [1.0, 1.0, 0.0, 1.0];
+pub const GREEN: Color = [0.0, 1.0, 0.0, 1.0];
+pub const CYAN: Color = [0.0, 1.0, 1.0, 1.0];
+pub const BLUE: Color = [0.0, 0.0, 1.0, 1.0];
+pub const MAGENTA: Color = [1.0, 0.0, 1.0, 1.0];
+pub const TRANSPARENT: Color = [0.0, 0.0, 0.0, 0.0];
+pub const BLACK: Color = [0.0, 0.0, 0.0, 1.0];
+pub const WHITE: Color = [1.0, 1.0, 1.0, 1.0];
 
-pub fn rgba_to_color(r: u8, g: u8, b: u8, a: u8) -> Color {
+pub fn rgba_to_normalized(r: u8, g: u8, b: u8, a: u8) -> Color {
     [
         ColorComponent::from(r) / 255.0,
         ColorComponent::from(g) / 255.0,
@@ -16,6 +18,6 @@ pub fn rgba_to_color(r: u8, g: u8, b: u8, a: u8) -> Color {
         ColorComponent::from(a) / 255.0,
     ]
 }
-pub fn rgb_to_color(r: u8, g: u8, b: u8) -> Color {
-    rgba_to_color(r, g, b, 255)
+pub fn rgb_to_normalized(r: u8, g: u8, b: u8) -> Color {
+    rgba_to_normalized(r, g, b, 255)
 }
